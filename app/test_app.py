9e7247fc55354971b216413ff827df7cccb908d1
Fernando-Z <fernando.fora@gmail.com>
2023-10-15 21:30:16 -0300

    fix(test): encode chars in test response

--- app/test_app.py
+++ app/test_app.py
@@ -2,5 +2,5 @@ from app import *
 
 def test_hello_world():
     response = app.test_client().get('/')
-    assert b"Olá, mundo!" in response.data
+    assert "Olá, mundo!".encode("UTF-8") in response.data
     assert response.status_code == 200
